bca065932262fe910d984fc89003272a8b91f724
sourav kumar ojha <sojha7724@.com>
2021-05-18 08:13:19 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const pool = require('./db');
+const PORT = process.env.PORT || 5000;
 //middlewares
 app.use(cors());
 app.use(express.json());
-
+// app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
+}
 //routes
-
 //create a todo
 app.post('/todos', async (req, res) => {
   try {
@@ -87,6 +92,6 @@ app.delete('/todos/:id', async (req, res) => {
     console.log(error.message);
   }
 });
-app.listen(5000, () => {
-  console.log('server running on port 5000');
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
